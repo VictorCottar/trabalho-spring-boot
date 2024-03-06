3c5bd4621939a80016adabe21db03f7708f5a2c2
VictorCottar <victorcottarms@outlook.com>
2024-03-05 21:15:37 -0300

    Primeiro commit

---
+++ src/main/java/com/example/trabalhoSpringBoot/model/Livros.java
@@ -0,0 +1,7 @@
+package com.example.trabalhoSpringBoot.model;
+
+public class Livros {
+
+
+
+}
---
+++ src/test/java/com/example/trabalhoSpringBoot/ExecutorTests.java
@@ -0,0 +1,13 @@
+package com.example.trabalhoSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExecutorTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
