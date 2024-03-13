479d4121a07bf20e9bd7fcaa973fd54896226cbc
VictorCottar <victorcottarms@outlook.com>
2024-03-12 22:54:22 -0300

    Classe abstrata Pessoa

--- src/main/java/com/example/trabalhoSpringBoot/model/Consulta.java
+++ src/main/java/com/example/trabalhoSpringBoot/model/Consulta.java
@@ -6,7 +6,6 @@ import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
-import java.util.Date;
 
 @Entity
 public class Consulta extends Paciente {
